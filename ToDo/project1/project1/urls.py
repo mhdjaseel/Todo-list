36f44bcc7b2800456026ef44a6316d29477803c6
mhdjaseel <mhdjaseel663@gmail.com>
2025-04-26 14:52:47 +0530

    list template create database model create

--- ToDo/project1/project1/urls.py
+++ ToDo/project1/project1/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from. import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
+    path('',views.home,name='home'),
 
 ]
--- ToDo/project1/project1/views.py
+++ ToDo/project1/project1/views.py
@@ -1,6 +1,10 @@
 from django import views
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .model import Todo
 
 def home(request):
+    if request.POST:
+        item=request.POST.get('todoInput')
+        Todo.objects.create(item=item)
+        return redirect('home')
     return render(request,'home.html')
